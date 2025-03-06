340f46681f5a9373f324953d6f3e1af6de08ffa1
mrbishalbaniya <mr.bishal.baniya@gmail.com>
2025-03-06 13:43:00 +0545

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Serve static files from the "public" folder
+app.use(express.static('../public'));
+
+io.on('connection', (socket) => {
+    console.log('A user connected:', socket.id);
+
+    // Relay signaling messages between peers
+    socket.on('signal', (data) => {
+        socket.broadcast.emit('signal', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected:', socket.id);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
